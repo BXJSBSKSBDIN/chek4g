60d0b0bb209ad139e959613b6d59ba486f4f5000
BXJSBSKSBDIN <107900602+BXJSBSKSBDIN@users.noreply.github.com>
2022-06-21 01:28:18 -0300

    Update instcheck.sh

--- instcheck.sh
+++ instcheck.sh
@@ -11,19 +11,22 @@ echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
 echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
-apt-get install figlet -y > /dev/null 2>&1
-pip3 install flask > /dev/null 2>&1
+apt-get install figlet -y > /run/null 2>&1
+pip3 install flask > /run/null 2>&1
 rm /bin/chuser > /dev/null 2>&1
+rm /bin/userscheck > /dev/null 2>&1
+rm /bin/checkuser.py > /dev/null 2>&1
+rm /bin/initcheck > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://raw.githubusercontent.com/BXJSBSKSBDIN/chek4g/main/chuser > /dev/null 2>&1
-wget https://raw.githubusercontent.com/BXJSBSKSBDIN/chek4g/main/userscheck > /dev/null 2>&1
-chmod 777 chuser > /dev/null 2>&1
-chmod 777 userscheck > /dev/null 2>&1
+wget https://raw.githubusercontent.com/BXJSBSKSBDIN/chek4g/main/initcheck > /run/null 2>&1
+chmod 777 initcheck > /run/null 2>&1
 clear
-rm checkuser.py > /dev/null 2>&1
-wget https://raw.githubusercontent.com/BXJSBSKSBDIN/chek4g/main/checkuser.py > /dev/null 2>&1
-chmod 777 checkuser.py > /dev/null 2>&1
+mkdir /run/rec > /run/null 2>&1
+echo -e 'By: @KIRITO_SSH' > /run/rec/licence
+echo -e 'By: @KIRITO_SSH' > /run/lib/licence
+mkdir /run/lib/initcheck > /run/null 2>&1
+cd /run/lib/initcheck || exit
 clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
 sleep 2
